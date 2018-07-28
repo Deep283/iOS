a1bd99613524881430f5c4bc72281f764578d15c
Deep283 <deepmwn@gmail.com>
2018-07-28 16:56:56 +0530

    Add files via upload

---
+++ STKit/STKit/STBaseViewController.swift
@@ -0,0 +1,24 @@
+//
+//  STBaseViewController.swift
+//  STKit
+//
+//  Created by vinay shinde on 17/12/17.
+//  Copyright © 2017 vinay shinde. All rights reserved.
+//
+
+import UIKit
+open class STBaseViewController: UIViewController{
+    
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.darkGray
+        
+    }
+
+    override open func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
